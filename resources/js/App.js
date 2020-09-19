7a3b13316c9f5d1ac4ceb3ab6db5e2bf4b151567
Sammy <sammyoziegbe@gmail.com>
2020-09-19 21:16:59 +0100

    initial signup with classes

--- resources/js/App.js
+++ resources/js/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
+import Register from './components/Register'
+
 
 function App() {
     return(
         <div>
             <h1>Hello</h1>
+            <Register />
         </div>
     )
 }
